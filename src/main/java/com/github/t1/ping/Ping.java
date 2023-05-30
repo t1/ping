f156719b8e2a594e3103ff2d3f5fc029082fb1e2
Ruediger zu Dohna <snackbox@sinntr.eu>
2023-05-30 08:06:21 +0200

    update wildfly-maven-plugin

--- src/main/java/com/github/t1/ping/Ping.java
+++ src/main/java/com/github/t1/ping/Ping.java
@@ -1,11 +1,8 @@
 package com.github.t1.ping;
 
-import lombok.extern.slf4j.Slf4j;
-
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import java.security.Principal;
+import lombok.extern.slf4j.Slf4j;
 
 @Path("/")
 @Slf4j
